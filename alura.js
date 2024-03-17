505d28a56e77e685a2d1060377bdb7c31907ff75
Flavio Zannoni <89528432+FlavioZannoni@users.noreply.github.com>
2024-03-17 19:23:03 -0300

    Add files via upload

---
+++ alura.js
@@ -0,0 +1,33 @@
+//https://cursos.alura.com.br/course/html-css-cabecalho-footer-variaveis-css/task/119367
+
+// DOCUMENTAÇÃO
+// https://www.w3schools.com/
+
+// cores 
+//https://colorhunt.co/palette/8e3200a64b2ad7a86effebc1
+
+
+//segunda paina
+
+// testo 
+//"A psicologia é o campo de estudo que investiga os processos mentais e comportamentais dos seres humanos, buscando compreender como pensamos, sentimos e agimos em diferentes contextos. Desde suas origens filosóficas até os desenvolvimentos modernos, a psicologia abrange uma ampla gama de teorias, métodos e aplicações práticas.
+
+// Desde Freud até os psicólogos contemporâneos, a disciplina evoluiu significativamente, incorporando diversas abordagens teóricas, como o behaviorismo, a psicologia cognitiva, a psicologia humanista e a psicologia social. Cada abordagem oferece uma perspectiva única sobre a mente humana e seu funcionamento.
+
+// Além de sua importância teórica, a psicologia desempenha um papel fundamental na prática clínica, ajudando indivíduos a lidar com questões emocionais, mentais e comportamentais. Os psicólogos clínicos, por exemplo, trabalham com pacientes para identificar e tratar transtornos mentais, enquanto os psicólogos educacionais ajudam a melhorar os processos de aprendizagem e desenvolvimento nas escolas.
+
+// A pesquisa em psicologia também contribui para nosso entendimento sobre uma variedade de tópicos, incluindo memória, emoção, motivação, percepção e desenvolvimento humano. Esses estudos são fundamentais para informar políticas públicas, práticas clínicas e intervenções sociais.
+
+// Em resumo, a psicologia desempenha um papel crucial na compreensão da mente humana e no aprimoramento do bem-estar individual e coletivo. Ao investigar os complexos processos mentais e comportamentais, a psicologia continua a oferecer insights valiosos que impactam diversas áreas da vida cotidiana e da sociedade como um todo."
+
+// NOMES PARA O ESCRITORRIO
+// PsicoThais Maria Gomes - Acolhimento Integral
+// Serenidade com Thais Maria Gomes - Escritório de Psicologia
+// Equilíbrio Mental - Thais Maria Gomes Psicóloga
+// VivaMente com Thais Maria Gomes - Consultório Psicológico
+// Trilhas da Mente - Thais Maria Gomes Psicologia
+// Essência PsicoThais Maria Gomes - Bem-Estar Emocional
+// Conexão Interior - Escritório de Psicologia Thais Maria Gomes
+// Mente Plena - Thais Maria Gomes Psicóloga Clínica
+// ->Raízes do Autoconhecimento - Thais Maria Gomes Psicologia
+// Inspiração Mental - Escritório de Psicologia Thais Maria Gomes
